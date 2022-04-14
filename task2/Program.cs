c6703495d8cbb4d0e43d8feea0e6c6e125d2fb6c
DanilaPestov <89147750075@mail.ru>
2022-04-14 23:42:42 +1000

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = 392;
+int numberB = 433;
+int numberC = 538;
+int max;
+if (numberA > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+if (numberC > max) max = numberC;
+Console.Write($"наибольшее число - {max} ");
